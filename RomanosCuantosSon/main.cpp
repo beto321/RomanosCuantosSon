ce9a200a76fa39d93352e4a224ea3a71739fa424
IvoPc <ivoalabe@gmail.com>
2016-04-10 22:16:12 -0400

    fix punto y coma

--- RomanosCuantosSon/main.cpp
+++ RomanosCuantosSon/main.cpp
@@ -15,10 +15,10 @@ void unidades(int n){
         case 3: R[I] += 3; break;
         case 4: R[I] += 1; R[V]+=1; break;
         case 5: R[V] += 1; break;
-        case 6: R[V] += 1; R[I]+=1 break;
-        case 7: R[V] += 1; R[I]+=2 break;
-        case 8: R[V] += 1; R[I]+=3 break;
-        case 9: R[I] += 1; R[X]+=1 break;
+        case 6: R[V] += 1; R[I]+=1; break;
+        case 7: R[V] += 1; R[I]+=2; break;
+        case 8: R[V] += 1; R[I]+=3; break;
+        case 9: R[I] += 1; R[X]+=1; break;
     }
 }
 
@@ -29,10 +29,10 @@ void decenas(int n){
         case 3: R[X] += 3; break;
         case 4: R[X] += 1; R[L]+=1; break;
         case 5: R[L] += 1; break;
-        case 6: R[L] += 1; R[X]+=1 break;
-        case 7: R[L] += 1; R[X]+=2 break;
-        case 8: R[L] += 1; R[X]+=3 break;
-        case 9: R[X] += 1; R[C]+=1 break;
+        case 6: R[L] += 1; R[X]+=1; break;
+        case 7: R[L] += 1; R[X]+=2; break;
+        case 8: R[L] += 1; R[X]+=3; break;
+        case 9: R[X] += 1; R[C]+=1; break;
         case 10: R[C] += 1; break;
     }
 }
