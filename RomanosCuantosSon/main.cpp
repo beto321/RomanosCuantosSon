36c6b27b2fed558f7d85c762404d4f0810903534
IvoPc <ivoalabe@gmail.com>
2016-04-10 21:32:26 -0400

    add metodos

--- RomanosCuantosSon/main.cpp
+++ RomanosCuantosSon/main.cpp
@@ -6,7 +6,40 @@
 #define L 3
 #define C 4
 
-using namespace std;
+int R[5];
+
+void unidades(int n){
+    switch(n){
+        case 1: R[I] += 1; break;
+        case 2: R[I] += 2; break;
+        case 3: R[I] += 3; break;
+        case 4: R[I] += 1; R[V]+=1; break;
+        case 5: R[V] += 1; break;
+        case 6: R[V] += 1; R[I]+=1 break;
+        case 7: R[V] += 1; R[I]+=2 break;
+        case 8: R[V] += 1; R[I]+=3 break;
+        case 9: R[I] += 1; R[X]+=1 break;
+    }
+}
+
+void decenas(int n){
+    switch(n){
+        case 1: R[X] += 1; break;
+        case 2: R[X] += 2; break;
+        case 3: R[X] += 3; break;
+        case 4: R[X] += 1; R[L]+=1; break;
+        case 5: R[L] += 1; break;
+        case 6: R[L] += 1; R[X]+=1 break;
+        case 7: R[L] += 1; R[X]+=2 break;
+        case 8: R[L] += 1; R[X]+=3 break;
+        case 9: R[X] += 1; R[C]+=1 break;
+        case 10: R[C] += 1; break;
+    }
+}
+
+
+
+
 
 int main()
 {
